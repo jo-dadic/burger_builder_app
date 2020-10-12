15e13b03874f5fb3911b1f5ba238a7bc6f88d5ea
Josipa Dadić <josipa.dadic@gmail.com>
2020-10-12 20:20:25 +0200

    burger added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <h1>Hello World</h1>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Aux from "../../hoc/Auxiliary"; //for wrapping the root element
 
+import classes from "./Layout.css";
+
 // we are using Layout comp as a wrappir around the core comp that we want to render to the screen
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
