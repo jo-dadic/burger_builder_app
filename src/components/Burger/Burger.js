3db70fda57be51f1a6caea57225be664333efe4b
Josipa Dadić <josipa.dadic@gmail.com>
2020-10-13 11:43:17 +0200

    empty ingredients mesagge added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const Burger = (props) => {
   // transforming an object of key-value pairs into the array of ingredients where the value of the object is important for to know many ingredients we need and the key is important for which type of the ingredients we need
   // keys() - extract keys from object and gives an array
-  const TransformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let TransformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  console.log(TransformedIngredients);
+
+  if (TransformedIngredients.length === 0) {
+    TransformedIngredients = <p>Please, start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ import Aux from "../../hoc/Auxiliary";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     },
   };
 
