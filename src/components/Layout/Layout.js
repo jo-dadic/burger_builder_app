0000e3f9d72bb045e027616908468d264d32371d
Josipa Dadić <josipa.dadic@gmail.com>
2020-10-16 10:24:20 +0200

    sidedrawer added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxiliary"; //for wrapping the root element
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 import classes from "./Layout.css";
 
 // we are using Layout comp as a wrappir around the core comp that we want to render to the screen
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
