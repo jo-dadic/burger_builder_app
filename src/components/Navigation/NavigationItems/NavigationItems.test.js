69e3be95e7b7d0a1360207e62f16472d2b84d4f8
Josipa Dadić <josipa.dadic@gmail.com>
2021-01-06 12:17:29 +0100

    testing added

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+//this is needed to connect and configure enzyme:
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  it("should render two <NavigationItem /> elements if not authenticated", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItem /> elements if authenticated", () => {
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it("should have a logout item if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
+});
